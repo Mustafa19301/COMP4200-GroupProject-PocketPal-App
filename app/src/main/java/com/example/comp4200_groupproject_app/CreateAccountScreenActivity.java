7d854226b520e2576461c9a5861e67c19a8fd041
Mustafa Ahmed <mustafaahmed3848@gmail.com>
2026-03-18 13:31:28 -0400

    Updated Create and Login logic, and added SQLite Database

--- app/src/main/java/com/example/comp4200_groupproject_app/CreateAccountScreenActivity.java
+++ app/src/main/java/com/example/comp4200_groupproject_app/CreateAccountScreenActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +16,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CreateAccountScreenActivity extends AppCompatActivity {
-    EditText firstnameview, lastnameview, emailview, phonenumberview, usernameview, passwordview;
+    EditText firstnameview, lastnameview, emailview, passwordview;
     ImageView imagelogoview;
     Button createbutton;
     TextView haveanaccount;
-
+    DBHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,12 +30,40 @@ public class CreateAccountScreenActivity extends AppCompatActivity {
         firstnameview = findViewById(R.id.firstnameview);
         lastnameview = findViewById(R.id.lastnameview);
         emailview = findViewById(R.id.emailview);
-        phonenumberview = findViewById(R.id.phonenumberview);
-        usernameview = findViewById(R.id.usernameview);
         passwordview = findViewById(R.id.passwordview);
         imagelogoview = findViewById(R.id.imagelogoview);
         createbutton = findViewById(R.id.createbutton);
         haveanaccount = findViewById(R.id.haveanaccount);
+        dbHelper = new DBHelper(this, "PocketPalUsers_database", null, 1);
+        dbHelper.getReadableDatabase();
+
+        createbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstname = firstnameview.getText().toString();
+                String lastname = lastnameview.getText().toString();
+                String email = emailview.getText().toString();
+                String password = passwordview.getText().toString();
+
+                if(firstname.isEmpty() || lastname.isEmpty() || email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(CreateAccountScreenActivity.this, "Please fill all required fields", Toast.LENGTH_LONG).show();
+                }
+
+                else {
+                    long result = dbHelper.adduser(firstname, lastname, email, password);
+
+                    if(result != -1){
+                        Toast.makeText(CreateAccountScreenActivity.this, "Account has been created", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(CreateAccountScreenActivity.this, LoginScreenActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(CreateAccountScreenActivity.this, "Account already exists!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
         haveanaccount.setOnClickListener(new View.OnClickListener() {
             @Override
